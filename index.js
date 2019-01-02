bb29d1c0dc3d4d9264b45e26f49b9c6b4e92f1cd
thompsonalecbgo <33586971+thompsonalecbgo@users.noreply.github.com>
2019-01-02 17:49:02 +0800

    Add files via upload
    javascript v2 (01/02/19)

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function toggleButton() {
   if (x.value === "shake dice") {
     x.value = "roll dice";
     diceAnimation = setInterval(shakeDice, 50);
+    return diceAnimation;
   } else {
     x.value = "shake dice";
     clearInterval(diceAnimation);
