ad130f7615b00c7898a467ff9a31813c2a7ef7f8
thompsonalecbgo <33586971+thompsonalecbgo@users.noreply.github.com>
2019-01-03 00:05:45 +0800

    Add files via upload
    using flexbox

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ function shakeDice() {
 function toggleButton() {
 //  var x = document.querySelector("input");
   var y = document.querySelector("button");
+  var x = document.querySelector(".button-text");
   if (y.value === "shake dice") {
     y.value = "roll dice";
-    y.innerHTML = "Roll Dice";
+    x.innerHTML = "Roll Dice";
     diceAnimation = setInterval(shakeDice, 50);
     diceAnimation;
   } else {
     y.value = "shake dice";
-    y.innerHTML = "Shake Dice";
+    x.innerHTML = "Shake Dice";
     clearInterval(diceAnimation);
   }
 }
